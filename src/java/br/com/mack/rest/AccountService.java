2bdec4f4123b59b298dbc9b88f20f4643d804f33
BrunoAlbuquerqueBrit <BrunoAlbuquerqueBrit@192.168.0.14>
2018-03-16 11:11:39 -0300

    A chamada dos metodos basico estao funcionando

--- src/java/br/com/mack/rest/AccountService.java
+++ src/java/br/com/mack/rest/AccountService.java
@@ -8,6 +8,9 @@ package br.com.mack.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
@@ -18,21 +21,26 @@ public class AccountService {
     
     
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/saldo/{conta}")
     public String saldo(){
         return "Saldo funcionando";
     }
     
     @POST
+    @Path("/deposito/{conta}/{valor}")
     public String deposito(){
         return "Deposito funcionando";
     }
     
     @POST
+    @Path("/transferencia/{contaOrigem}/{contaDestino}/{valor}")
     public String transferencia(){
         return "Transferencia funcionando";
     }
     
     @POST
+    @Path("/saque/{conta}/{valor}")
     public String saque(){
         return "Saque funcionando";
     }
